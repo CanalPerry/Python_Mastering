7c4715b4e8e0d225c708ad7acd5853d366232cfd
CanalPerry <canalp3rry@gmail.com>
2021-02-17 20:38:50 -0300

    master.0.4

---
+++ Exe2.py
@@ -0,0 +1,16 @@
+'''
+2 - Escreva um Programa que escreva na tela, de 1 até 100 ,de 1 em 1 ,3 vezes
+> Primeira- Deve usar estrutura de repetição For.
+> Segunda- Deve usar estrutara de repetição while
+'''
+'''
+#Primeira
+for i in range(0,101):
+    print(i)
+
+'''
+#Segunda
+i=0
+while i< 101:
+    print(i)
+    i += 1
