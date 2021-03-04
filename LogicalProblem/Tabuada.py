483dace1f36f3f0107736b4239990d6e576d3f4e
CanalPerry <canalp3rry@gmail.com>
2021-03-04 08:04:45 -0300

    master.3.4

--- LogicalProblem/Tabuada.py
+++ LogicalProblem/Tabuada.py
@@ -1,2 +1,9 @@
-tabuada = [[f"{y} x {x} = {x * y}"for y in range(1, 10)]for x in range(1, 10)]
-[print(valor)for valor in tabuada]
+cont = 0
+[[print('')if y == 10 else print(f"{x} x {y} = {x * y}")for y in range(1, 10+1)]for x in range(1, 10+1)]
+
+
+
+
+
+
+
