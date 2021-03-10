8df7af125a4e0cbff23677c86d0124d3955b7dc2
Sir-Arthur <canalp3rry@gmail.com>
2021-03-10 16:49:48 -0300

    master 4.0

---
+++ Udemy-Nivel4/Ex01/Ex01.py
@@ -0,0 +1,8 @@
+with open('Tabuada.txt', 'w') as arq:
+    arq.write('------ TABUDA:--------\n')
+    for i in range(1, 11):
+        for x in range(1, 11):
+            arq.write(f'-- {i} x {x} = {i * x} ')
+            arq.write('\n')
+        arq.write('-----------------\n')
+
